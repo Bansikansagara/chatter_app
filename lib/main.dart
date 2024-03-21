5a097a33702ee231034c6376869363887d0f6f54
LENOVO <bansikansagara164@gmail.com>
2024-03-22 00:15:54 +0530

    latest code deploy in repor

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:chatter/Pages/FirstPage.dart';
+import 'package:chatter/Pages/LoginPage.dart';
+
+
+void main()
+{
+  //Launch FirstScreen - SplashScreen
+  runApp(MaterialApp(home:FirstPage()));
+}
+
